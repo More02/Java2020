dacbcaafe749aef925f77dc50078896403c4e319
Unripper1 <dmharitoh@gmail.com>
2020-09-14 12:07:53 +0300

    5th and 6th pract

---
+++ src/ru/more02/ex6/Movable.java
@@ -0,0 +1,8 @@
+package ru.more02.ex6;
+
+public interface Movable {
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
---
+++ src/ru/more02/ex6/MovablePoint.java
@@ -0,0 +1,33 @@
+package ru.more02.ex6;
+
+import ru.more02.ex5.Movable;
+
+public class MovablePoint implements Movable {
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+    public void moveUp() {}
+    public void moveDown(){}
+    public void moveLeft(){}
+    public void moveRight(){}
+
+}
