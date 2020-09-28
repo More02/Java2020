73bcc6f3b2473c94ed287c05abd43e0c9072877c
Marina <mbakanova2016@yandex.ru>
2020-09-29 00:18:17 +0300

    15th finished well

--- src/ru/more02/ex15/Main.java
+++ src/ru/more02/ex15/Main.java
@@ -27,14 +27,15 @@ public class Main {
             System.out.print(ss+" ");
         }
         MyArrayList<Integer> myArrayList = new  MyArrayList<Integer>();
-        myArrayList.add(4);
         myArrayList.add(5);
-        myArrayList.add(6);
+        myArrayList.add(5);
         myArrayList.add(7);
+        myArrayList.add(6);
         myArrayList.add(8);
 
+        System.out.println();
         for (int i=0;i<myArrayList.size();i++) {
-            System.out.println(myArrayList.get(i));
+            System.out.print(myArrayList.get(i)+ " ");
         }
 
 
